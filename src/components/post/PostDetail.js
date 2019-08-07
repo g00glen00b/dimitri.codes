873d30fcb3ccc82b3f99fa356b26eac68c545115
g00glen00b <me@g00glen00b.be>
2019-08-07 10:46:10 +0200

    Added proper inline image handling

--- src/components/post/PostDetail.js
+++ src/components/post/PostDetail.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import {PostTags} from './PostTags';
 import {PageTitle, PostMeta} from '../../theme';
+import parse from 'html-react-parser';
+import {PostImage} from './PostImage';
+
+const replaceImages = node => {
+  if (node.name === 'img') {
+    return <PostImage src={node.attribs.src} alt={node.attribs.alt}/>;
+  }
+};
 
 export const PostDetail = ({title, readingTime: {text}, content, tags, date}) => (
   <article>
@@ -8,6 +16,6 @@ export const PostDetail = ({title, readingTime: {text}, content, tags, date}) =>
       <PageTitle>{title}</PageTitle>
       <PostMeta>{date} | {text} | <PostTags tags={tags}/></PostMeta>
     </header>
-    <div dangerouslySetInnerHTML={{__html: content}}/>
+    <div>{parse(content, {replace: replaceImages})}</div>
   </article>
 );
---
+++ src/components/post/PostImage.js
@@ -0,0 +1,35 @@
+import {graphql, useStaticQuery} from 'gatsby';
+import Img from 'gatsby-image';
+import React from 'react';
+
+const allMedia = graphql`
+  query {
+    allWordpressWpMedia {
+      edges {
+        node {
+          source_url
+          localFile {
+            publicURL
+            childImageSharp {
+              fluid(maxWidth: 800) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const PostImage = ({src, alt}) => {
+  const {allWordpressWpMedia} = useStaticQuery(allMedia);
+  const originalSource = src.replace(/^(http?s:\/\/.+?\/.+?)-(\d+x\d+)\.(.+?)$/g, '$1.$3');
+  const image = allWordpressWpMedia.edges.find(({node}) => node.source_url === originalSource);
+
+  return image == null ? (
+    <span>Not found '{src}'</span>
+  ) : (
+    <Img fluid={image.node.localFile.childImageSharp.fluid} alt={alt}/>
+    );
+};
