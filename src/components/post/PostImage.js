3666fd3babe70312878ebbd99f55d4818cd5bede
g00glen00b <me@g00glen00b.be>
2019-08-08 15:28:52 +0200

    ..

--- src/components/post/PostImage.js
+++ src/components/post/PostImage.js
@@ -1,6 +1,7 @@
-import {graphql, useStaticQuery} from 'gatsby';
+import {graphql, Link, useStaticQuery} from 'gatsby';
 import Img from 'gatsby-image';
 import React from 'react';
+import styled from '@emotion/styled';
 
 const allMedia = graphql`
   query {
@@ -26,7 +27,6 @@ export const PostImage = ({src, alt, width}) => {
   const {allWordpressWpMedia} = useStaticQuery(allMedia);
   const originalSource = src.replace(/^(http?s:\/\/.+?\/.+?)-(\d+x\d+)\.(.+?)$/g, '$1.$3');
   const image = allWordpressWpMedia.edges.find(({node}) => node.source_url === originalSource);
-  console.log(width);
   return image == null || image.node.localFile.childImageSharp == null ? (
     <img
       src={src}
