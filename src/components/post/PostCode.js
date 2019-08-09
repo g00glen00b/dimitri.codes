85c805bd4cdbda1824900498742e85a7f35abe94
g00glen00b <me@g00glen00b.be>
2019-08-09 09:46:50 +0200

    Removed logging of each code blocks language

--- src/components/post/PostCode.js
+++ src/components/post/PostCode.js
@@ -2,13 +2,10 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import {github} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-export const PostCode = ({language, children}) => {
-  console.log(language);
-  return (
-    <SyntaxHighlighter
-      style={github}
-      language={language}>
-      {children}
-    </SyntaxHighlighter>
-  );
-};
+export const PostCode = ({language, children}) => (
+  <SyntaxHighlighter
+    style={github}
+    language={language}>
+    {children}
+  </SyntaxHighlighter>
+);
