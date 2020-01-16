f0d15d99a02f1ce0365596b47fa3893e1e40aeaf
g00glen00b <me@g00glen00b.be>
2020-01-16 08:52:27 +0100

    Removed old components

--- src/components/shared/SimpleLink.js
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-import {Link} from 'gatsby';
-import React from 'react';
-
-export const SimpleLink = styled(props => <Link {...props}/>)`
-  text-decoration: none;
-  color: ${({theme}) => theme.textColor};
-`;
--- src/components/site/SiteIdentity.js
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-
-export const SiteIdentity = styled.div`
-  display: flex;
-  align-items: center;
-`;
