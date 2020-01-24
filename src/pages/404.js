1e14d85efb3659a4e8ee9b314b3b6b9d4f39fa41
g00glen00b <me@g00glen00b.be>
2020-01-24 11:52:02 +0100

    Added layout to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {SEO} from '../components/Seo';
+import {Layout} from '../components/Layout';
 
 
 const NotFoundPage = () => (
-  <main>
+  <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </main>
+  </Layout>
 );
 
 export default NotFoundPage;
