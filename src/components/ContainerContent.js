6081659c82f34d74f5c4aa8e6dc7bb10c71ad382
g00glen00b <me@g00glen00b.be>
2020-07-04 22:38:48 +0200

    Moved content specific CSS to a new content container component

---
+++ src/components/ContainerContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ContainerContent.css';
+import PropTypes from 'prop-types';
+
+export const ContainerContent = ({children}) => (
+  <main className="container__content">
+    {children}
+  </main>
+);
+
+ContainerContent.propTypes = {
+  children: PropTypes.object
+};
