8281022fbd0150d652fc0e9cf9c0999453213e4a
g00glen00b <dimitri.mestdagh@optis.be>
2022-11-09 14:52:21 +0100

    fix: fixed import issue

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,4 +1,3 @@
-import {OutboundLink} from 'gatsby-plugin-google-analytics';
 import {FaCodepen, FaGithub, FaSpeakerDeck, FaTwitter, FaMastodon} from 'react-icons/fa';
 import React from 'react';
 import './SocialLinks.css';
@@ -6,46 +5,46 @@ import PropTypes from 'prop-types';
 
 export const SocialLinks = ({socialNetworks}) => (
   <nav className="social-links">
-    {socialNetworks.twitter && <OutboundLink
+    {socialNetworks.twitter && <a
       className="social-link"
       href={`https://twitter.com/${socialNetworks.twitter}`}
       target="_blank"
       rel="noopener noreferrer"
       title="Twitter">
       <FaTwitter size={24}/>
-    </OutboundLink>}
-    {socialNetworks.mastodon && <OutboundLink
+    </a>}
+    {socialNetworks.mastodon && <a
       className="social-link"
       href={`https://mastodon.cloud/@${socialNetworks.mastodon}`}
       target="_blank"
       rel="me noopener noreferrer"
       title="Mastodon">
       <FaMastodon size={24}/>
-    </OutboundLink>}
-    {socialNetworks.github && <OutboundLink
+    </a>}
+    {socialNetworks.github && <a
       className="social-link"
       href={`https://github.com/${socialNetworks.github}`}
       target="_blank"
       rel="noopener noreferrer"
       title="GitHub">
       <FaGithub size={24}/>
-    </OutboundLink>}
-    {socialNetworks.speakerdeck && <OutboundLink
+    </a>}
+    {socialNetworks.speakerdeck && <a
       className="social-link"
       href={`https://speakerdeck.com/${socialNetworks.speakerdeck}`}
       target="_blank"
       rel="noopener noreferrer"
       title="Speaker Deck">
       <FaSpeakerDeck size={24}/>
-    </OutboundLink>}
-    {socialNetworks.codepen && <OutboundLink
+    </a>}
+    {socialNetworks.codepen && <a
       className="social-link"
       href={`https://codepen.io/${socialNetworks.codepen}`}
       target="_blank"
       rel="noopener noreferrer"
       title="CodePen">
       <FaCodepen size={24}/>
-    </OutboundLink>}
+    </a>}
   </nav>
 );
 
